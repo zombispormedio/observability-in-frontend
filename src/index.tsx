8ff66a57659fb507ae2e96b1b6e5a86bf18475ed
Xavier Serrano <zombispormedio007@gmail.com>
2022-09-21 20:51:40 +0200

    :bug: fix service worker registration

--- src/index.tsx
+++ src/index.tsx
@@ -142,7 +142,7 @@ const Presentation = () => (
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("service-worker.js")
       .then((registration) => {
         console.log("SW registered: ", registration);
       })
