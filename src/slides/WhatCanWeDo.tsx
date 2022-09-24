ca65636c28306153f59ff60b8876a0a99857de6a
Xavier Serrano <zombispormedio007@gmail.com>
2022-09-24 17:23:45 +0200

    :art: merge what can we do

--- src/slides/WhatCanWeDo.tsx
+++ src/slides/WhatCanWeDo.tsx
@@ -1,81 +1,49 @@
 import React from "react";
-import { FlexBox, Appear, Text, Heading, Slide, Image, Notes } from "spectacle";
-import GrafanaDashboard from "../images/grafana-dashboard.png";
-import DatadogDashboard from "../images/datadog-dashboard.png";
-import SessionReplayDatadog from "../images/session-replay-datadog.jpg";
+import {
+  FlexBox,
+  Appear,
+  Text,
+  Heading,
+  Slide,
+  Notes,
+  UnorderedList,
+  ListItem,
+} from "spectacle";
 
 export const WhatCanWeDoSlides = () => {
   return (
-    <>
-      <Slide className="text-white">
-        <FlexBox
-          justifyContent="center"
-          alignContent="center"
-          height="100%"
-          flexDirection="column"
-        >
-          <Heading fontSize="h3">What can we do?</Heading>
+    <Slide className="text-white">
+      <Heading fontSize="h3">What could we do?</Heading>
+      <div className="flex justify-center">
+        <UnorderedList>
           <Appear>
-            <FlexBox justifyContent="center" alignContent="center">
-              <Text>Grafana?</Text>
-            </FlexBox>
+            <ListItem>
+              <Text>Error reporting?</Text>
+            </ListItem>
           </Appear>
           <Appear>
-            <Text>Datadog?</Text>
+            <ListItem>
+              <Text>Dashboards?</Text>
+            </ListItem>
           </Appear>
           <Appear>
-            <Text>More dashboards?</Text>
+            <ListItem>
+              <Text>Session Replays?</Text>
+            </ListItem>
           </Appear>
-          <Appear>
-            <div className="absolute left-0 right-0 top-0 flex items-center justify-center h-full">
-              <Image src={DatadogDashboard} width={1000} />
-            </div>
-          </Appear>
-          <Appear>
-            <div className="absolute left-0 right-0 top-0 flex items-center justify-center h-full rotate-12">
-              <Image src={GrafanaDashboard} width={1000} />
-            </div>
-          </Appear>
-        </FlexBox>
-        <Notes>
-          And what can we do to solve it? We add Grafana, Datadog? Good tools.
-          And we spend a lot of time creating, observing, and analysing more and
-          more complicated dashboards, with different metrics and pie charts and
-          gauges.
-        </Notes>
-      </Slide>
-      <Slide className="text-white">
-        <FlexBox
-          justifyContent="center"
-          alignContent="center"
-          height="100%"
-          flexDirection="column"
-        >
-          <Heading fontSize="h3">What can we do in the frontend?</Heading>
-          <Appear>
-            <Text>Sentry?</Text>
-          </Appear>
-          <Appear>
-            <Text>Datadog?</Text>
-          </Appear>
-          <Appear>
-            <Text>More user session recordings?</Text>
-          </Appear>
-          <Appear>
-            <div className="absolute left-0 right-0 top-0 flex items-center justify-center h-full rotate-12">
-              <Image src={SessionReplayDatadog} width={1000} />
-            </div>
-          </Appear>
-        </FlexBox>
-        <Notes>
-          But, to be realistic, what we can do in the front end? really. Do we
-          add an error reporting system like Sentry? And sitting waiting for
-          exceptions. Sentry is a wonderful tool, the best for error tracking,
-          it gives a lot of context. I pay for it. Datadog again? Datadog does a
-          lot of things. Do we record more user sessions to understand why the
-          user is suffering? Is it enough? Help us? Yes, no, sometimes.
-        </Notes>
-      </Slide>
-    </>
+        </UnorderedList>
+      </div>
+      <Notes>
+        And what can we do to solve it? We add Grafana, Datadog? Good tools. And
+        we spend a lot of time creating, observing, and analysing more and more
+        complicated dashboards, with different metrics and pie charts and
+        gauges. But, to be realistic, what we can do in the front end? really.
+        Do we add an error reporting system like Sentry? And sitting waiting for
+        exceptions. Sentry is a wonderful tool, the best for error tracking, it
+        gives a lot of context. I pay for it. Datadog again? Datadog does a lot
+        of things. Do we record more user sessions to understand why the user is
+        suffering? Is it enough? Help us? Yes, no, sometimes.
+      </Notes>
+    </Slide>
   );
 };
